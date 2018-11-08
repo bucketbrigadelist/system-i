068d3194197b9c7bb23bf5c9f192ddecfaca9f52
bucketbrigadelist <test@digitalcredis.co.uk>
2018-11-08 14:23:25 -0500

    Ignore

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 console.log("jQuery v"+jQuery.fn.jquery);
 
+$(document).ready(function() {
+    // $('#elements').load('includes/elements.html');
+    $('#blocks').load('includes/blocks.html');
+});
