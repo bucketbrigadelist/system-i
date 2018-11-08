a89cb43a671b5ed0362b1688c41c74c8f2dfb0cd
bucketbrigadelist <test@digitalcredis.co.uk>
2018-11-08 15:49:36 -0500

    Ignore

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 console.log("jQuery v"+jQuery.fn.jquery);
 
+// $( "div" ).click(function() {
+//   var fontname = $( this ).css( "font-family" );
+//   $( "#inforesult" ).html( "That font is " +
+//     fontname);
+// });
+
 $(document).ready(function() {
-    // $('#elements').load('includes/elements.html');
-    $('#blocks').load('includes/blocks.html');
+    $('#header-i').load('includes/header.html');
+    $('#elements').load('includes/elements.html');
+    // $('#blocks').load('includes/blocks.html');
+    // $('#fonts').load('includes/fonts.html');
+    // $('#contentend').load('includes/contentend.html');
+    $('#footer-i').load('includes/footer.html');
 });
